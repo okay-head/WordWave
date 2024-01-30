775af1b605da6aeb8c9c5457a3b5fa05802c9f56
okay-head <68184623+okay-head@users.noreply.github.com>
2024-01-30 13:48:45 +0530

    add global types

---
+++ src/types/types.d.ts
@@ -0,0 +1,9 @@
+type userPayload = {
+  user_id: string
+  user_name: string
+  user_handle: string
+  user_bio: string
+  user_tweets: string[]
+  user_followers: string[]
+  user_following: string[]
+}
