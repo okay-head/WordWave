068d192ffa1a01fbf629aee73efe61a66337cd6c
okay-head <68184623+okay-head@users.noreply.github.com>
2024-01-31 11:57:15 +0530

    fix: persist global state in session storage

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -4,8 +4,12 @@ import useGlobalStore from '../state/GlobalState'
 export default function Logout() {
   const { setAuth } = useGlobalStore()
   signOutFn().then(() => {
+    // TODO: display some cool loading animations in here
     setTimeout(() => {
       setAuth(false)
+      // clear all localstorage
+      localStorage.clear()
+      sessionStorage.clear()
     }, 500)
   })
   return (
--- src/components/state/GlobalState.ts
+++ src/components/state/GlobalState.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 type Tuser = {
   uid: string
   email: string
@@ -9,11 +10,19 @@ type T = {
   setAuth: (val: boolean) => void
   setfirebaseAuthObj: (val: Tuser) => void
 }
-const useGlobalStore = create<T>()((set) => ({
-  auth: false, //is authenticated
-  firebaseAuthObj: { uid: 'null', email: 'null' }, //firebase auth object
-  setAuth: (val) => set(() => ({ auth: val })),
-  setfirebaseAuthObj: (val) => set(() => ({ firebaseAuthObj: val })),
-}))
+const useGlobalStore = create<T>()(
+  persist(
+    (set) => ({
+      auth: false, //is authenticated
+      firebaseAuthObj: { uid: 'null', email: 'null' }, //firebase auth object
+      setAuth: (val) => set(() => ({ auth: val })),
+      setfirebaseAuthObj: (val) => set(() => ({ firebaseAuthObj: val })),
+    }),
+    {
+      name: 'global-store',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+)
 
 export default useGlobalStore
