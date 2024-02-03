05b99904d4da3fc5d4320e07fa207c8a8d644d05
okay-head <68184623+okay-head@users.noreply.github.com>
2024-02-03 12:06:03 +0530

    fix: logout component handles state and storage resets

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,19 +1,25 @@
 import { signOutFn } from '../../firebase/firebaseAuth'
 import useGlobalStore from '../state/GlobalState'
+import placeHolderDataUser from '../user/placeHolderData'
 
 export default function Logout() {
-  const { setAuth } = useGlobalStore()
+  const setAuth = useGlobalStore((state) => state.setAuth)
+  const setUser = useGlobalStore((state) => state.setUser)
   signOutFn().then(() => {
     // TODO: display some cool loading animations in here
     setTimeout(() => {
+      // reset state
+      setUser(placeHolderDataUser[0])
       setAuth(false)
+
       // clear all localstorage
       localStorage.clear()
       sessionStorage.clear()
     }, 500)
   })
   return (
-    <div className='absolute inset-0 grid place-content-center bg-gray-100'>
+    // additional 1vh height to prevent scrollbar jump layout shift
+    <div className='absolute inset-0 grid min-h-[101vh] place-content-center bg-gray-100'>
       <h1 className='text-4xl font-semibold'>Logging out ...</h1>
     </div>
   )
