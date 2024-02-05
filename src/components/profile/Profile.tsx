4e0dcd872808c88a04475ead736f09a7b7f9ed8e
okay-head <68184623+okay-head@users.noreply.github.com>
2024-02-05 12:06:50 +0530

    feat: profile page is realtime

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from 'react'
 import placeHolderData from '../feed/placeholderData'
 import Card from '../shared/Card'
 import Container from '../shared/Container'
+import placeHolderDataUser from '../user/placeHolderData'
 import UserCard from '../users/UserCard'
+import useGlobalStore from '../state/GlobalState'
+import { getFn } from '../../firebase/firebaseDb'
 // import TabBtn from './TabBtn'
 
 export default function Profile() {
+  const [followers, setFollowers] = useState(placeHolderDataUser)
+  const [following, setFollowing] = useState(placeHolderDataUser)
+  const [posts, setPosts] = useState(placeHolderData)
+
+  const userObjContext = useGlobalStore((state) => state.user)
+  useEffect(() => {
+    /* 1. POSTS */
+    // make a get call to all tweets
+    const tweets = userObjContext.user_tweets
+    if (tweets[0] == '0') setPosts([])
+
+    /* 2. FOLLOWERS */
+    const followers = userObjContext.user_followers
+    if (followers[0] == '0') setFollowers([])
+
+    /* 3. FOLLOWING */
+    const following = userObjContext.user_following
+    console.log(following)
+
+    if (following[0] == '0') setFollowing([])
+    else {
+      // Promise.all(following.map((x) => getFn(`users/${x}`)))
+      Promise.all(following.map((x) => getFn(`users/${x}`)))
+        .then((res) => setFollowing(res))
+        .catch((err) => console.log(err))
+    }
+  }, [userObjContext])
   return (
     <Container classVars='max-w-2xl mt-10'>
       <article className='header-card card'>
@@ -19,29 +50,43 @@ export default function Profile() {
               <div className='flex items-center gap-3'>
                 <div>
                   <h3 className='w-full text-3xl font-semibold text-gray-800 dark:text-white'>
-                    Author name
+                    {userObjContext.user_name}
                   </h3>
                   <p className='-mt-[2px] text-sm font-medium text-accent-pink-500 dark:text-accent-pink-900'>
-                    @username
+                    {userObjContext.user_handle}
                   </p>
                 </div>
               </div>
             </section>
 
             <div className='mt-2 text-gray-500 dark:text-gray-400'>
-              <p> Crypto. Investing. Market manipulation.</p>
-              <p> All opinions are mine</p>
+              {userObjContext.user_bio}
             </div>
 
             <div className='chips mt-7 flex gap-4 text-sm text-gray-500 dark:text-gray-400'>
               <span className='border-r pr-5'>
-                <b className='font-semibold'>10</b> Posts{' '}
+                <b className='font-semibold'>
+                  {userObjContext.user_tweets[0] == '0'
+                    ? 0
+                    : userObjContext.user_tweets?.length}
+                </b>{' '}
+                Post(s){' '}
               </span>
               <span className='border-r pr-5'>
-                <b className='font-semibold'>450</b> Followers{' '}
+                <b className='font-semibold'>
+                  {userObjContext.user_followers[0] == '0'
+                    ? 0
+                    : userObjContext.user_followers?.length}
+                </b>{' '}
+                Follower(s){' '}
               </span>
               <span>
-                <b className='font-semibold'>750</b> Following{' '}
+                <b className='font-semibold'>
+                  {userObjContext.user_following[0] == '0'
+                    ? 0
+                    : userObjContext.user_following?.length}
+                </b>{' '}
+                Following{' '}
               </span>
             </div>
           </div>
@@ -127,9 +172,22 @@ export default function Profile() {
             aria-labelledby='bar-with-underline-item-1'
           >
             <div className='cards-container panel-1-content'>
-              {placeHolderData.map((x, i) => {
-                return <Card key={i} {...x} />
-              })}
+              {posts.length == 0 ? (
+                <div className=''>
+                  <div className='mt-16 flex flex-col gap-4'>
+                    <img
+                      src='/assets/no-data.svg'
+                      alt='No user tweets'
+                      className='mx-auto w-[33%]'
+                    />
+                    <h2 className='text-center text-lg'>No Posts</h2>
+                  </div>
+                </div>
+              ) : (
+                posts.map((x, i) => {
+                  return <Card key={i} {...x} />
+                })
+              )}
             </div>
           </div>
           <div
@@ -139,9 +197,22 @@ export default function Profile() {
             aria-labelledby='bar-with-underline-item-2'
           >
             <div className='cards-container panel-2-content'>
-              {[1, 2].map((_, i) => {
-                return <UserCard key={i} />
-              })}
+              {followers.length == 0 ? (
+                <div>
+                  <div className='mt-16 flex flex-col gap-4'>
+                    <img
+                      src='/assets/no-followers.svg'
+                      alt='No Followers'
+                      className='mx-auto w-[40%]'
+                    />
+                    <h2 className='text-center text-lg'>No followers</h2>
+                  </div>
+                </div>
+              ) : (
+                followers.map((x, i) => {
+                  return <UserCard key={i} {...x} />
+                })
+              )}
             </div>
           </div>
           <div
@@ -151,9 +222,22 @@ export default function Profile() {
             aria-labelledby='bar-with-underline-item-3'
           >
             <div className='cards-container panel-2-content'>
-              {[1, 2, 3, 4, 5].map((_, i) => {
-                return <UserCard key={i} />
-              })}
+              {following.length == 0 ? (
+                <div>
+                  <div className='mt-16 flex flex-col gap-4'>
+                    <img
+                      src='/assets/no-followers.svg'
+                      alt='No Following'
+                      className='mx-auto w-[40%]'
+                    />
+                    <h2 className='text-center text-lg'>No following</h2>
+                  </div>
+                </div>
+              ) : (
+                following.map((x, i) => {
+                  return <UserCard key={i} {...x} />
+                })
+              )}
             </div>
           </div>
         </div>
